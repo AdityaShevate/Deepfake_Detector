547b221ae06088d8db2720310841a20dcc780e4f
Ishani Badhe <ishani.badhe@cloudburytech.com>
2026-06-07 00:51:00 +0530

    Created upload UI with image preview

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+
 function App() {
+  const [file, setFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+
   return (
     <div>
-      <h1>Deepfake Detection Web App</h1>
-      <button>Upload Image</button>
+      <h1>Deepfake Detector</h1>
+
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(event) => {
+          const selectedFile =
+            event.target.files[0];
+
+          setFile(selectedFile);
+
+          if (selectedFile) {
+            setPreview(
+              URL.createObjectURL(selectedFile)
+            );
+          }
+        }}
+      />
+
+      <p>
+        {file
+          ? file.name
+          : "No file selected"}
+      </p>
+
+      {preview && (
+        <img
+          src={preview}
+          alt="Preview"
+          width="300"
+        />
+      )}
+
+      <br />
+      <br />
+
+      <button>Detect</button>
     </div>
   );
 }
