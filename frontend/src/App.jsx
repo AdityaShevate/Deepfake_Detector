c3d0d6fb22b590be458d5ae795a70c5ae9579ec5
Ishani Badhe <ishani.badhe@cloudburytech.com>
2026-06-20 02:49:46 +0530

    Add result UI, loading state, Axios setup, and component refactoring

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,55 @@
+import { useState } from "react";
+import DetectButton from "./components/DetectButton";
+import LoadingSpinner from "./components/LoadingSpinner";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import UploadButton from "./components/UploadButton";
 import ImageUpload from "./components/ImageUpload";
 import ResultCard from "./components/ResultCard";
 
 function App() {
+
+  const [prediction, setPrediction] = useState("No Result");
+  const [confidence, setConfidence] = useState(0);
+  const [loading, setLoading] = useState(false);
+
   return (
     <div>
       <Header />
+
       <ImageUpload />
-      <button>Detect</button>
-      <ResultCard />
+
+      <DetectButton
+  onDetect={() => {
+
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+  }}
+/>
+{
+  loading && (
+    <p>Analyzing Image...</p>
+  )
+}
+<LoadingSpinner/>
+
+      <button
+        onClick={() => {
+          setPrediction("Fake");
+          setConfidence(93);
+        }}
+      >
+        Test Result
+      </button>
+
+      <ResultCard
+        prediction={prediction}
+        confidence={confidence}
+      />
+
       <Footer />
     </div>
   );
--- frontend/src/components/ResultCard.jsx
+++ frontend/src/components/ResultCard.jsx
@@ -1,12 +1,33 @@
-function ResultCard() {
+function ResultCard(props) {
   return (
     <div className="result-card">
 
       <h2>Result</h2>
 
-      <p>Prediction: Fake</p>
+      {
+        props.prediction === "Fake" ? (
+          <h3>🔴 Fake</h3>
+        ) : props.prediction === "Real" ? (
+          <h3>🟢 Real</h3>
+        ) : (
+          <h3>No Result</h3>
+        )
+      }
 
-      <p>Confidence: 90%</p>
+      <p>
+        Confidence: {props.confidence}%
+      </p>
+
+      <div className="progress-bar">
+
+        <div
+          className="progress-fill"
+          style={{
+            width: `${props.confidence}%`
+          }}
+        ></div>
+
+      </div>
 
     </div>
   );
