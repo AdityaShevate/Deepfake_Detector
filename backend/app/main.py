83bac1739d152a657a0b159f867991fde65d970d
Aditya Jaydeep Shevate <adityashevate@Adityas-MacBook-Air.local>
2026-06-22 19:45:00 +0530

    feat: implement image upload endpoint in FastAPI

---
+++ backend/app/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from fastapi import UploadFile
+from fastapi import File
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message": "Deepfake Detection API Running"
+    }
+
+@app.post("/upload")
+async def upload_image(
+    file: UploadFile = File(...)
+):
+    allowed_extensions = (
+    ".jpg",
+    ".jpeg",
+    ".png"
+)
+    if not file.filename.lower().endswith(
+    allowed_extensions
+):return {
+        "error": "Only JPG, JPEG and PNG files are allowed"
+    }
+    
+    try:
+        contents = await file.read()
+
+        with open(
+            f"uploads/{file.filename}",
+            "wb"
+        ) as f:
+            f.write(contents)
+
+        return {
+            "message": "Upload Successful",
+            "filename": file.filename
+        }
+    
+    except Exception as e:
+        return{
+        "error": str(e)
+        }    
+
+    size = len(contents)
+
+    return {
+        "message":"Upload Successful",
+        "filename": file.filename,
+        "size": size
+    }
